5141e31dc8888900eb351694ca4b3e2744dab2d1
Davide Morra <davide.morra@volcanicminds.com>
2025-12-19 10:34:09 +0100

    remove require loading

--- index.ts
+++ index.ts
@@ -24,68 +24,64 @@ import * as log from './lib/util/logger.js'
 import yn from './lib/util/yn.js'
 
 async function start(options) {
-  return new Promise<DataSource>((resolve, reject) => {
-    if ((global as any).npmDebugServerStarted) {
-      options = {
-        type: 'postgres',
-        host: '127.0.0.1',
-        port: 5432,
-        username: 'vminds',
-        password: 'vminds',
-        database: 'vminds',
-        synchronize: true,
-        logging: true, // query, error, schema, warn, info, all
-        logger: '' // advanced-console, simple-console
-      }
+  if ((global as any).npmDebugServerStarted) {
+    options = {
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'vminds',
+      password: 'vminds',
+      database: 'vminds',
+      synchronize: true,
+      logging: true, // query, error, schema, warn, info, all
+      logger: '' // advanced-console, simple-console
     }
+  }
 
-    if (options == null || Object.keys(options).length == 0) {
-      throw new Error('Volcanic Database: options not specified')
-    }
+  if (options == null || Object.keys(options).length == 0) {
+    throw new Error('Volcanic Database: options not specified')
+  }
 
-    const { LOG_DB_LEVEL = 'warn', LOG_COLORIZE = true, DB_SYNCHRONIZE_SCHEMA_AT_STARTUP = false } = process.env
+  const { LOG_DB_LEVEL = 'warn', LOG_COLORIZE = true, DB_SYNCHRONIZE_SCHEMA_AT_STARTUP = false } = process.env
 
-    const logLevel: string | boolean =
-      LOG_DB_LEVEL === 'trace'
-        ? 'all'
-        : LOG_DB_LEVEL === 'debug'
-        ? 'query'
-        : LOG_DB_LEVEL === 'info'
-        ? 'info'
-        : LOG_DB_LEVEL === 'warn'
-        ? 'warn'
-        : LOG_DB_LEVEL === 'error'
-        ? 'error'
-        : LOG_DB_LEVEL
+  const logLevel: string | boolean =
+    LOG_DB_LEVEL === 'trace'
+      ? 'all'
+      : LOG_DB_LEVEL === 'debug'
+      ? 'query'
+      : LOG_DB_LEVEL === 'info'
+      ? 'info'
+      : LOG_DB_LEVEL === 'warn'
+      ? 'warn'
+      : LOG_DB_LEVEL === 'error'
+      ? 'error'
+      : LOG_DB_LEVEL
 
-    ;(global as any).cacheTimeout = options?.cacheTimeout || 30000 // milliseconds
-    ;(global as any).isLoggingEnabled = options?.logging || true
+  ;(global as any).cacheTimeout = options?.cacheTimeout || 30000 // milliseconds
+  ;(global as any).isLoggingEnabled = options?.logging || true
 
-    const { classes, repositories, entities } = loaderEntities.load()
-    options.entities = [...(options.entities || []), ...(entities || [])]
-    options.logger = LOG_COLORIZE ? 'advanced-console' : 'simple-console'
-    options.logging = logLevel
-    options.synchronize = false
+  const { classes, repositories, entities } = await loaderEntities.load()
+  options.entities = [...(options.entities || []), ...(entities || [])]
+  options.logger = LOG_COLORIZE ? 'advanced-console' : 'simple-console'
+  options.logging = logLevel
+  options.synchronize = false
 
-    return new DataSource(options)
-      .initialize()
-      .then(async (ds) => {
-        if (yn(DB_SYNCHRONIZE_SCHEMA_AT_STARTUP, false)) {
-          log.warn('Database schema synchronization started')
-          await ds.synchronize()
-          log.warn('Database schema synchronization finished')
-        }
+  const ds = new DataSource(options)
+  await ds.initialize()
 
-        // load uselful stuff
-        const repository = {}
-        Object.keys(repositories).map((r) => (repository[r] = ds.getRepository(repositories[r])))
-        ;(global as any).connection = ds
-        ;(global as any).entity = classes
-        ;(global as any).repository = repository
-        return resolve(ds)
-      })
-      .catch((error) => reject(error))
-  })
+  if (yn(DB_SYNCHRONIZE_SCHEMA_AT_STARTUP, false)) {
+    log.warn('Database schema synchronization started')
+    await ds.synchronize()
+    log.warn('Database schema synchronization finished')
+  }
+
+  // load uselful stuff
+  const repository = {}
+  Object.keys(repositories).map((r) => (repository[r] = ds.getRepository(repositories[r])))
+  ;(global as any).connection = ds
+  ;(global as any).entity = classes
+  ;(global as any).repository = repository
+  return ds
 }
 
 export { Database } from './types/global.js'
--- lib/loader/entities.ts
+++ lib/loader/entities.ts
@@ -2,35 +2,42 @@
 import * as path from 'path'
 import { globSync } from 'glob'
 import pluralize from 'pluralize'
-import { createRequire } from 'module'
-import { fileURLToPath } from 'url'
+import { fileURLToPath, pathToFileURL } from 'url'
 
-const require = createRequire(import.meta.url)
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-export function load() {
+export async function load() {
   const classes: any = {}
   const repositories: any = {}
   const entities: any[] = []
 
   const patterns = [
     path.join(__dirname, '..', 'entities', '*.e.{ts,js}'),
-    path.join(process.cwd(), 'src', 'entities', '*.e.{ts,js}')
+    path.join(process.cwd(), 'src', 'entities', '*.e.{ts,js}'),
+    path.join(process.cwd(), 'dist', 'src', 'entities', '*.e.{ts,js}')
   ]
 
-  patterns.forEach((pattern) => {
-    globSync(pattern, { nodir: true, windowsPathsNoEscape: true }).forEach((f: string) => {
-      const entityClass = require(f)
+  for (const pattern of patterns) {
+    const files = globSync(pattern, { nodir: true, windowsPathsNoEscape: true })
+
+    for (const f of files) {
+      const fileUrl = pathToFileURL(f).href
+      const entityModule = await import(fileUrl)
+
+      const entityClass = entityModule.default || entityModule
       const entityNames = Object.keys(entityClass)
 
-      entityNames.map((name) => {
-        classes[name] = entityClass[name]
-        repositories[pluralize(name.toLowerCase())] = entityClass[name]
-        entities.push(entityClass[name])
+      entityNames.forEach((name) => {
+        const entity = entityClass[name]
+        if (typeof entity === 'function') {
+          classes[name] = entity
+          repositories[pluralize(name.toLowerCase())] = entity
+          entities.push(entity)
+        }
       })
-    })
-  })
+    }
+  }
 
   return { classes, repositories, entities }
 }
